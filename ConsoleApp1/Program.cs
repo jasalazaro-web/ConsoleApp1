d575a490aa5e614d2bbe6004f16a8fd912e651fa
jasalazaro-web <jasalazaro@unincca.edu.co>
2026-02-19 21:23:32 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Usando GIT v1");
+
+Console.WriteLine("Johana Andrea Salazar Ortiz");
